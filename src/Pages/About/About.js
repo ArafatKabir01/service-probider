3ddafbfeb725f3187da7f715b7c10679c00dbf82
ak nabil <nabil786.ak@gmail.com>
2022-04-18 23:53:35 +0600

    about page added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -6,7 +6,7 @@ const About = () => {
         <div className=' container'>
             <div className=''>
             <img className='w-25' src={myimg}></img>
-                <h2>Atafat Ibne Kabir</h2>
+                <h2>Arafat Ibne Kabir</h2>
                 <p>My goal is full stack web developer based on MERN.I love web developing.I want to build my career on this program,so i work hard very much.My daily learning time 9 to 10 hours every day.I believe i can achieve my goal</p>
             </div>
             
