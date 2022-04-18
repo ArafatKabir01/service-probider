4a626996d2df57d25e6008dff275c6432d8f8ac6
ak nabil <nabil786.ak@gmail.com>
2022-04-18 14:21:09 +0600

    404 page aded

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Booking from './Pages/Booking/Booking';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
+import NotFoundPage from './Pages/NotFoundPage/NotFoundPage';
 import RequireAuth from './Pages/RequireAuth/RequireAuth';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
@@ -18,6 +19,7 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/signup' element={<Signup></Signup>}></Route>
+        <Route path='*' element={<NotFoundPage></NotFoundPage>}></Route>
         <Route path='/booking' element={
           <RequireAuth>
             <Booking></Booking>
---
+++ src/Pages/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            <h1 className='fw-bolder fs-1 text-center'>Page Not Found 404</h1>
+        </div>
+    );
+};
+
+export default NotFoundPage;
