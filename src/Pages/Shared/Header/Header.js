f7ca3e4b2c07f4cfcf5bf8612401e8befbd4c998
ak nabil <nabil786.ak@gmail.com>
2022-05-15 13:55:35 +0600

    header added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
     return (
         <Navbar sticky='top' collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-            <Navbar.Brand className='text-info fs-4' href="#home">Doctor Room</Navbar.Brand>
+            <Navbar.Brand className='text-info fs-4' href="#home">Laptop WareHouse</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
